52d501724aa389b373d140de09ef00a5dbb9341b
Augusto <augustoespinoz4@gmail.com>
2025-06-12 20:04:29 -0300

    Botón de logout en el perfil

--- app/tabs/profile/index.tsx
+++ app/tabs/profile/index.tsx
@@ -1,8 +1,10 @@
 import { RecipeCard } from "@/components/recipes/RecipeCard";
 import useAuthStore from "@/store/authStore";
 import { Ionicons } from "@expo/vector-icons";
+import { router } from "expo-router";
 import React, { useState } from "react";
 import {
+  Alert,
   Image,
   SafeAreaView,
   ScrollView,
@@ -15,26 +17,45 @@ import {
 const UserProfileScreen = () => {
   const { user } = useAuthStore();
   const [avatarError, setAvatarError] = useState(false);
-  // const [pizzaImageError, setPizzaImageError] = useState(false);
+
+  const handleLogoutPress = () => {
+    Alert.alert(
+      "Cerrar sesión",
+      "¿Estás seguro de cerrar sesión?",
+      [
+        { text: "Cancelar", style: "cancel" },
+        {
+          text: "Cerrar sesión",
+          style: "destructive",
+          onPress: () => {
+            useAuthStore.getState().logout();
+            router.replace("/auth/login");
+          },
+        },
+      ],
+      { cancelable: true }
+    );
+  };
 
   return (
     <SafeAreaView className="flex-1 bg-white">
       <StatusBar barStyle="dark-content" backgroundColor="white" />
+
       {/* Header */}
-      <View className="px-4 py-4 border-b border-gray-200">
-        <Text className="text-xl font-bold text-gray-800 text-center">
+      <View className="px-4 py-4 border-b border-gray-200 flex-row items-center justify-between">
+        <Text className="text-xl font-bold text-gray-800 text-center flex-1">
           Perfil del usuario
         </Text>
+        <TouchableOpacity onPress={handleLogoutPress}>
+          <Ionicons name="log-out-outline" size={24} color="#000" />
+        </TouchableOpacity>
       </View>
+
       <ScrollView className="flex-1" showsVerticalScrollIndicator={false}>
         {/* Profile Header Section */}
         <View className="relative mb-8">
-          {/* Rectangulo gris del usuario */}
-          <View className="bg-gray-200 h-32 mx-4 rounded-2xl flex-row items-center justify-end relative mt-4">
-            {/* //Todo Imagen de fondo del perfil */}
-          </View>
+          <View className="bg-gray-200 h-32 mx-4 rounded-2xl flex-row items-center justify-end relative mt-4" />
 
-          {/* User Avatar - Positioned below y sobre el rectángulo gris */}
           <View className="absolute left-8 top-24">
             {!avatarError ? (
               <View className="flex-row items-center justify-center">
@@ -65,9 +86,7 @@ const UserProfileScreen = () => {
         {/* User Info */}
         <View className="px-4 mb-6 mt-2">
           <View className="flex-row items-center justify-between">
-            <Text className="text-2xl font-bold text-gray-800">
-              {user.name}
-            </Text>
+            <Text className="text-2xl font-bold text-gray-800">{user.name}</Text>
             <View className="flex-row items-center">
               <Ionicons name="star" size={18} color="#FFD700" />
               <Text className="text-lg font-semibold text-gray-800 ml-1">
@@ -79,7 +98,6 @@ const UserProfileScreen = () => {
 
         {/* Sección recetas */}
         <View className="px-4">
-          {/* Header */}
           <View className="flex-row items-center justify-between mb-4">
             <Text className="text-lg font-bold text-gray-800">Tus Recetas</Text>
             <View className="flex-row">
