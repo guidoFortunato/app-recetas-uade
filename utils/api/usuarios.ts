5b357ee12481f281589ab48c2082588fbc8bd9ab
Guido <46787485+guidoFortunato@users.noreply.github.com>
2025-07-03 21:41:37 -0300

    agrego console logs de errores

--- utils/api/usuarios.ts
+++ utils/api/usuarios.ts
@@ -153,7 +153,7 @@ export const agregarRecetasFavoritas = async (
       idUsuario,
       idReceta,
     });
-    console.log({ response: response.data });
+    // console.log({ response: response.data });
     return response.data;
   } catch (error) {
     console.error("Error en obtener recetas favoritas:", error);
